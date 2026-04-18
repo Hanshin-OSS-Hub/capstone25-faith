5621ae4039d437df268eaeafc0943ae60feb7e5b
wanghee1 <gmldnjs0321@hs.ac.kr>
2026-04-18 15:07:41 +0900

    Delete src directory

--- src/backend/app/db/init_mongo.py
+++
@@ -1,7 +0,0 @@
-# 인덱스 생성
-from app.db.nosql import mongo_db
-from pymongo import ASCENDING, DESCENDING
-
-def init_mongo():
-    mongo_db.guest_verification_log.create_index([("user_id", ASCENDING)])
-    mongo_db.guest_verification_log.create_index([("created_at", DESCENDING)])
--- src/backend/app/db/nosql.py
+++
@@ -1,8 +0,0 @@
-import os
-from pymongo import MongoClient
-
-MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
-MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "capstone_db")
-
-client = MongoClient(MONGO_URL)
-mongo_db = client[MONGO_DB_NAME]
