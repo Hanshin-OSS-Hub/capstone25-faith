2707737a090647753c6e0a727157c0e3ec605f67
wanghee1 <gmldnjs0321@hs.ac.kr>
2026-05-04 04:57:15 +0900

    Delete backend directory

--- backend/app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .members import Member
-from .verification_history import VerificationHistory
-from .risk_detail import RiskDetail
-from .user_profile import UserProfile
-from .archive_item import ArchiveItem
-from .archive_favorite import ArchiveFavorite
